6e7ff035d0eae319af45bdd206e39f175232714d
Victor Sindato <victor@dhcp-10-29-193-210.dyn.mit.edu>
2020-08-04 21:54:18 -0400

    Analytics for v2

--- Video Model/v2/js/face_process.js
+++ Video Model/v2/js/face_process.js
@@ -11,9 +11,9 @@ JSSDK.Assets = {
 var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
 var detector = new affdex.FrameDetector(faceMode);
 detector.detectEmotions.sadness = true;
+detector.detectExpressions.browFurrow = true;
 detector.detectEmotions.engagement = true;
-detector.detectExpressions.browRaise = true;
-detector.detectExpressions.innerBrowRaise = true;
+detector.detectEmotions.smile = true;
 detector.assets = JSSDK.Assets["wasm"];
 detector.start();
 
@@ -23,22 +23,76 @@ var container = document.createElement('div');
 container.innerHTML = "<button class='footer__leave-btn ax-outline' type='button' onclick='onStop()'><span class='footer__leave-btn-text'>Get Analysis</span></button>";
 footer.appendChild(container);
 
+// emotions and expressions being tracked
+var sadness_ = [];
+var confusion_ = [];
+var engagement_ = [];
+var smile_ = [];
+var framenum = [];
+var ind = 0;
+
+//function executes when the Stop button is pushed.
+function onStop() {
+  if (detector && detector.isRunning) {
+    detector.removeEventListener();
+    detector.stop();
+    console.log('STOPPED');
+    document.querySelector('#zmmtg-root').className = "hidden";
+
+    var sadnessChart = {
+      x: framenum,
+      y: sadness_,
+      name: 'Sadness',
+      type: 'lines'
+    };
+
+    var confusionChart = {
+      x: framenum,
+      y: confusion_,
+      name: 'Confusion',
+      type: 'lines'
+    };
+
+    var engagementChart = {
+      x: framenum,
+      y: engagement_,
+      name: 'Engagement',
+      type: 'lines'
+    };
+
+    var smileChart = {
+      x: framenum,
+      y: smile_,
+      name: 'Smile',
+      type: 'lines'
+    };
+
+    var data4 = [sadnessChart, confusionChart, engagementChart, smileChart];
+
+    var layout = {
+      title: 'Patient\'s state through out the session',
+      xaxis: {
+        title: 'Frame number'
+      },
+      yaxis: {
+        title: 'Emotion detected',
+        showline: false
+      }
+    };
+
+    Plotly.newPlot('analytics', data4, layout);
+
+    //re-setting all arrays
+    sadness_ = [];
+    confusion_ = [];
+    engagement_ = [];
+    smile_ = [];
+  //  framenum = [];
+  }
+};
+
 //Cache the timestamp of the first frame processed
 var startTimestamp = (new Date()).getTime() / 1000;
-
-detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
-  console.log("FINISHED PROCESSING");
-  console.log(faces);
-  if (faces.length > 0) {
-    if(document.querySelector('#face_video_canvas') != null)
-      drawFeaturePoints(image, faces[0].featurePoints);
-      console.log("DRAWS FEATURE POINTS");
-  }
-});
-
-detector.addEventListener("onImageResultsFailure", function(faces, image, timestamp) {
-  console.log("FAILED PROCESSING");
-});
 
 detector.addEventListener("onInitializeSuccess", function() {
   console.log("INITIALIZE SUCCESS");
@@ -69,8 +123,6 @@ detector.addEventListener("onInitializeSuccess", function() {
     var deltaTime = now - startTimestamp;
     //Process the frame
     await draw(imageData, deltaTime).then(setTimeout(repeat, 500));
-    // detector.process(imageData, deltaTime);
-    // console.log("GOT TO PROCESSING");
 
   })();
 });
@@ -79,65 +131,35 @@ detector.addEventListener("onInitializeFailure", function() {
   console.log("FAILED INITIALIZING");
 });
 
-//function executes when the Stop button is pushed.
-function onStop() {
-  if (detector && detector.isRunning) {
-    detector.removeEventListener();
-    detector.stop();
-    console.log('STOPPED');
-    document.querySelector('#zmmtg-root').className = "hidden";
 
-  //   var smileChart = {
-  //     x: framenum,
-  //     y: smile_,
-  //     name: 'Smile',
-  //     type: 'lines'
-  //   };
-  //
-  //   var attentionChart = {
-  //     x: framenum,
-  //     y: attention_,
-  //     name: 'Attention',
-  //     type: 'lines'
-  //   };
-  //
-  //   var talkingChart = {
-  //     x: framenum,
-  //     y: talking_,
-  //     name: 'Talking',
-  //     type: 'lines'
-  //   };
-  //
-  //   var valenceChart = {
-  //     x: framenum,
-  //     y: valence_,
-  //     name: 'Valence',
-  //     type: 'lines'
-  //   };
-  //
-  //   var data4 = [smileChart, attentionChart, talkingChart, valenceChart];
-  //
-  //   var layout = {
-  //     title: 'Emotions displayed through out the session',
-  //     xaxis: {
-  //       title: 'Frame number'
-  //     },
-  //     yaxis: {
-  //       title: 'Emotion detected',
-  //       showline: false
-  //     }
-  //   };
-  //
-  //   Plotly.newPlot('analytics', data4, layout);
-  //
-  //   //re-setting all arrays
-  //   smile_ = [];
-  //   attention_ = [];
-  //   talking_ = [];
-  //   valence_ = [];
-  // //  framenum = [];
+detector.addEventListener("onImageResultsSuccess", function(faces, image, timestamp) {
+  console.log("FINISHED PROCESSING");
+  console.log(faces);
+  if (faces.length > 0) {
+    if(document.querySelector('#face_video_canvas') != null) {
+      drawFeaturePoints(image, faces[0].featurePoints);
+      console.log("DRAWS FEATURE POINTS");
+    }
+    ind += 1;
+    framenum.push(ind);
+    sadness_.push(faces[0].emotions['sadness'].toFixed(2));
+    confusion_.push(faces[0].expressions['browFurrow'].toFixed(2));
+    engagement_.push(faces[0].emotions['engagement'].toFixed(2));
+    smile_.push(faces[0].expressions['smile'].toFixed(2));
+  } else {
+    ind += 1;
+    framenum.push(ind);
+    sadness_.push(0);
+    confusion_.push(0);
+    engagement_.push(0);
+    smile_.push(0);
   }
-};
+});
+
+detector.addEventListener("onImageResultsFailure", function(faces, image, timestamp) {
+  console.log("FAILED PROCESSING");
+});
+
 
 //Draw the detected facial feature points on the image
 function drawFeaturePoints(img, featurePoints) {
